b70a751ed924a3aca430f2407eef61c07a43334b
FORHAD HOSSAIN <111189539+Forhadhossaincub@users.noreply.github.com>
2022-08-13 22:32:14 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_a_circle_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double radius;
+            double area;
+
+            radius = 10.0;
+
+            area = radius * radius * 3.1416;
+
+            Console.WriteLine("Circle area is " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
